2a63970e935d67e3ea590a28d80fdccbb526d045
keisuke2020 <kei_a_study@yahoo.co.jp>
2020-08-15 16:59:15 +0900

    既読機能のサーバーサイド実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
   post 'posts', to: 'posts#create'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'posts/:id', to: 'posts#checked'
 end
